5daab1a393a06c998e273489ca8dea5394c879e5
Daniel Ramirez <123519249+danielramirez611@users.noreply.github.com>
2024-08-27 11:05:01 -0500

    update4

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php
 	function conexion(){
 
-	$host = "host=dpg-cr6g04d6l47c73f3her0-a";
+	$host = "host=dpg-cr6g04d6l47c73f3her0-a.oregon-postgres.render.com";
 	$port = "port=5432";
-	$dbname = "dbname=databaseprueba";
+	$dbname = "dbname=databse_zkc6";
 	$user = "user=databse_zkc6_user";
 	$password = "password=Djf3X3DMcTQLNmKzIkLp2t6IK5rpUxn0";
 
